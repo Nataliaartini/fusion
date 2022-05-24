c402142c25f9696784d93ec6f812f301bf0f183e
Natalia <nataliaartini@hotmail.com>
2022-05-24 13:43:15 -0300

    arrumando testes do forms

--- core/tests/test_forms.py
+++ core/tests/test_forms.py
@@ -5,26 +5,10 @@ from core.forms import ContactForm
 class ContactFormTest(TestCase):
 
     def test_valid_data(self):
-        self.name = 'Test Name'
-        self.email = 'a@a.com'
-        self.subject = 'Test Subject'
-        self.message = 'Test Message'
-
-        self.content = {
-            'name': self.name,
-            'email': self.email,
-            'subject': self.subject,
-            'message': self.message
-        }
-        self.form = ContactForm(data=self.content)  #ContactForm(request.POST)
-
-    def test_send_mail(self):
-        form1 = ContactForm(data=self.content)
-        form1.is_valid()
-        res1 = form1.send_mail()
-
-        form2 = self.form
-        form2.is_valid()
-        res2 = form2.send_mail()
-
-        self.assertEqual(res1, res2)
+        form = ContactForm(data={
+            'name': "Test Name",
+            'email': "a@a.com",
+            'subject': "Test Subject",
+            'message': "Test Message"
+        })
+        self.assertTrue(form.is_valid()) #ContactForm(request.POST)
