1da99077842ba1ab53c4798cf4fc685d2e0a48ee
Natalia <nataliaartini@hotmail.com>
2022-05-26 16:20:02 -0300

    customizando visualização model admin e subindo migration

--- models/outro/models.py
+++ models/outro/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 class Post(models.Model):
-    author = models.ForeignKey(get_user_model(), Verbose_name="Autor", on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), verbose_name="Autor", on_delete=models.CASCADE)
     title = models.CharField(max_length=200, verbose_name="Título")
     text = models.TextField(verbose_name="Texto", max_length=1000)
 
