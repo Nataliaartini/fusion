106884cc0e38edba372f3400037405ebcb39e566
Natalia <nataliaartini@hotmail.com>
2022-05-26 15:31:28 -0300

    customizando user model

--- models/outro/models.py
+++ models/outro/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(get_user_model(), Verbose_name="Autor", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, verbose_name="Título")
+    text = models.TextField(verbose_name="Texto", max_length=1000)
+
+    def __str__(self):
+        return self.title
