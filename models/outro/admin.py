508210811acc79afb7d78dcb5332715371499ba4
Natalia <nataliaartini@hotmail.com>
2022-05-27 17:07:09 -0300

    criando filtros de request user para posts

--- models/outro/admin.py
+++ models/outro/admin.py
@@ -4,4 +4,16 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author')
+    list_display = ('title', '_author')
+    exclude = ['author']
+
+    def _author(self, instance):
+        return instance.author.username
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(author=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
